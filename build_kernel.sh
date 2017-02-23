efac18f09a10c37f91c5b35eba46fba7ac2a7916
tmthang <tmthang86@live.com>
2017-02-23 15:22:17 +0700

    build_kernel: Fix-up building
    Change-Id: I408590725575037e459b20c444ddda9c3ac0b4fd
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,12 @@
 
 #!/bin/bash
 
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
+export PATH=$(pwd)/toolchain/aarch64-linux-android-4.9/bin:$PATH
+export ARCH=arm64
 # export SEC_BUILD_OPTION_HW_REVISION=02
 
 mkdir out
 
-make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android VARIANT_DEFCONFIG=msm8976_sec_a9xprolte_sea_defconfig msm8976_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig 
+make -C $(pwd) O=$(pwd)/out CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android VARIANT_DEFCONFIG=msm8976_sec_a9xprolte_sea_defconfig msm8976_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig 
 
-make -j64 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
-
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
-
+make -j$(nproc --all) -C $(pwd) O=$(pwd)/out CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
